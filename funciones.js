eb957b4d566fd74afa1d641de1f7687844e30358
VinicioAyalaC <vhac2311@gmail.com>
2026-04-15 21:31:08 -0500

    Capacidad de pago

--- funciones.js
+++ funciones.js
@@ -1,1 +1,14 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
+//Calcular Disponible
+function calcularDisponible(ingresos, egresos){
+    let disponible = ingresos - egresos;
+    if(disponible < 0){
+        disponible = 0;
+    }
+    return disponible;
+}
+
+function calcularCapacidadDePago(montoDisponible){
+    let capacidadDePago= montoDisponible/2;
+    return capacidadDePago;
+}
--- simulador.js
+++ simulador.js
@@ -1,1 +1,14 @@
-//AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML
+//AQUI EL JAVASCRIPT PARA MANIPULAR EL HTML 
+
+function calcular() {
+    let ingresos = parseFloat(document.getElementById("txtIngresos").value);
+    let egresos = parseFloat(document.getElementById("txtEgresos").value);
+
+    let disponible = calcularDisponible(ingresos, egresos);
+    const elemento = document.getElementById("spnDisponible");
+    elemento.innerText = disponible;
+
+    let capacidadDePago = calcularCapacidadDePago(disponible);
+    const elementoA = document.getElementById("spnCapacidadPago");
+    elemento.innerText = capacidadDePago;
+}
