fc41e5d8e25bb06ad7863221a6f38d21fd6d7de5
VinicioAyalaC <vhac2311@gmail.com>
2026-04-17 19:38:43 -0500

    Calcular monto solicitado + intereses

--- funciones.js
+++ funciones.js
@@ -1,5 +1,8 @@
 //AQUI TODA LA LOGICA DE LAS FUNCIONES DEL NEGOCIO
 //Calcular Disponible
+
+const IMPUESTOS=100;
+
 function calcularDisponible(ingresos, egresos){
     let disponible = ingresos - egresos;
     if(disponible < 0){
@@ -8,7 +11,22 @@ function calcularDisponible(ingresos, egresos){
     return disponible;
 }
 
+
 function calcularCapacidadDePago(montoDisponible){
     let capacidadDePago= montoDisponible/2;
     return capacidadDePago;
+}
+
+
+function calcularInteresSimple(monto,tasa,plazoAnios){
+    //interes = plazo*monto*(tasa/100)
+    let interes = plazoAnios*monto*(tasa/100);
+    return interes;
+}
+
+
+function calcularTotalPagar(montoSol,interesGen){
+    //SUMAR MONTO SOLICITADO + INTERES + IMPUESTOS
+    let totalPagar = montoSol + interesGen + IMPUESTOS;
+    return totalPagar;
 }
--- simulador.js
+++ simulador.js
@@ -6,9 +6,25 @@ function calcular() {
 
     let disponible = calcularDisponible(ingresos, egresos);
     const elemento = document.getElementById("spnDisponible");
-    elemento.innerText = disponible;
+    elemento.innerText = disponible.toFixed(2);
 
     let capacidadDePago = calcularCapacidadDePago(disponible);
     const CapacidadPago = document.getElementById("spnCapacidadPago");
-    CapacidadPago.innerText = capacidadDePago;
+    CapacidadPago.innerText = capacidadDePago.toFixed(2);
+
+    // Solicitud de credito
+    let montoSolicitado = parseInt( document.getElementById("txtMonto").value);
+    let plazoAnios =  parseInt(document.getElementById("txtPlazo").value);
+    let tasaAnualSimple = parseInt(document.getElementById("txtTasaInteres").value);
+
+    let valorInteres =  calcularInteresSimple(montoSolicitado,tasaAnualSimple,plazoAnios);
+    const interesPagar = document.getElementById("spnInteresPagar");
+    interesPagar.innerText = valorInteres.toFixed(2);
+
+
+    // total a pagar del credito + intereses
+    let totalPagarCredito = calcularTotalPagar(montoSolicitado,valorInteres);
+    const totalCredito = document.getElementById("spnTotalPrestamo");
+    totalCredito.innerText = totalPagarCredito.toFixed(2);
+
 }
